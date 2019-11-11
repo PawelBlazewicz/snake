90a02c8eb64f63110b9cd497004c234bc2e390ad
PawelBlazewicz <52447280+PawelBlazewicz@users.noreply.github.com>
2019-11-11 11:09:13 +0100

    added food

--- app.js
+++ app.js
@@ -1,36 +1,29 @@
 const cvs = document.getElementById('cvs').getContext('2d');
 //cvs.fillText("snake", 100, 100);
+cvs.font = "20px";
 const Width = 500;
 const Height =500;
-//cvs.height = Height;
-cvs.font = "20px";
-let snakeList= [];
-let foods= [];
+let snake = [
+    {x:240, y:200},
+    {x:220, y:200},
+    {x:200, y:200}
+];
+let food= {};
 
+const gridSize = 20;
 
-const snakeCell ={
-    width: 18,
-    height: 18,
-    color:'green'
-};
-
-const food = {
-    width: 20,
-    hight: 20,
-    color: 'orange'
-};
 let move = 20;
 let direction = [0, move];
 document.addEventListener('keydown', function(event){
     switch(event.keyCode) {
-        case 37: 
-            direction = [-move, 0]; break;
-        case 38: 
-            direction = [0, -move]; break;
+        case 37 || 65: 
+            if (direction[0] != move ) {direction = [-move, 0];} break;
+        case 38 || 87: 
+            if (direction[1] != move ) {direction = [0, -move];} break;
         case 39: 
-            direction = [move, 0]; break;
+            if (direction[0] != -move ) {direction = [move, 0];} break;
         case 40: 
-            direction = [0, move]; break;
+            if (direction[1] != -move ) {direction = [0, move];} break;
     }
 } );
 
@@ -39,52 +32,52 @@ const drawSnake =(snake, i)=>{
     if (i===0){
       cvs.fillStyle = 'red';
     } else {
-        cvs.fillStyle = snakeCell.color;
+        cvs.fillStyle = "green";
     }
-    cvs.fillRect(snake.x, snake.y, snakeCell.width, snakeCell.height);
+    cvs.fillRect(snake.x+1, snake.y+1, gridSize-2, gridSize-2);
     cvs.restore();
 };
 
-const drawFood =(f, i)=>{
+const makeFood = () =>{
+    let x = Math.floor(Math.random() * 480);
+    x -= x%20;
+    let y = Math.floor(Math.random() * 480);
+    y -= y%20;
+    food = {x:x, y:y};
+    console.log(food);
+}
+
+const drawFood =(food)=>{
     cvs.save();
-    cvs.fillStyle = food.color;
-    cvs.fillRect(f.x, f.y, food.width, food.height);
+    cvs.fillStyle = "orange";
+    cvs.fillRect(food.x+1, food.y+1, gridSize-2, gridSize-2);
     cvs.restore();
 };
 
 const updateSnake =()=> {
-    for (let i=snakeList.length-1; i >= 0; --i){
-        if (snakeList[i].x === 0) {snakeList[i].x = 500}
-        if (snakeList[i].y === 0) {snakeList[i].y = 500}
+    for (let i=snake.length-1; i >= 0; --i){
+        if (snake[i].x === 0) {snake[i].x = 500}
+        if (snake[i].y === 0) {snake[i].y = 500}
         if (i === 0){
-            snakeList[i].x = (snakeList[i].x + direction[0])%Height;
-            snakeList[i].y = (snakeList[i].y + direction[1])%Width;
+            snake[i].x = (snake[i].x + direction[0])%Height;
+            snake[i].y = (snake[i].y + direction[1])%Width;
         } else {
-            snakeList[i].x = (snakeList[i-1].x)%Height;
-            snakeList[i].y = (snakeList[i-1].y)%Width;
+            snake[i].x = (snake[i-1].x)%Height;
+            snake[i].y = (snake[i-1].y)%Width;
         }
     } 
 } 
 
-const updateSnakePosition =()=> {
-    cvs.fillRect(0,0,Width,Height);    
-    snakeList.forEach(drawSnake);
+const updateBoard =()=> {
+    cvs.clearRect(0,0,Width,Height);    
+    snake.forEach(drawSnake);
+    drawFood(food);
     updateSnake();    
 }
 
-
-
 const newGame =()=> {
-    snakeList = [
-        {x:240, y:200},
-        {x:220, y:200},
-        {x:200, y:200}
-    ];
-    foods = [];
-    //console.log("fef");
-    snakeList.forEach(drawSnake);
-    setInterval(updateSnakePosition,500);
-    
+    makeFood();
+    setInterval(updateBoard,100);    
 } 
 
 newGame();
