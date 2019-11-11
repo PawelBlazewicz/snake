2ec217a367f553990841e40dfd05de6c355178bd
PawelBlazewicz <52447280+PawelBlazewicz@users.noreply.github.com>
2019-11-11 17:55:59 +0100

    Working game

--- app.js
+++ app.js
@@ -1,63 +1,63 @@
 const cvs = document.getElementById('cvs').getContext('2d');
-//cvs.fillText("snake", 100, 100);
-cvs.font = "20px";
 const Width = 500;
 const Height =500;
-let snake = [
-    {x:240, y:200},
-    {x:220, y:200},
+const gridSize = 25;
+
+const snakeStart = [
+    {x:200 + 2* gridSize, y:200},
+    {x:200 + gridSize, y:200},
     {x:200, y:200}
 ];
+let snake;
 let food= {};
 
-const gridSize = 20;
-
-let move = 20;
+let move = gridSize;
 let direction = [0, move];
 document.addEventListener('keydown', function(event){
     switch(event.keyCode) {
-        case 37 || 65: 
+        case 37: 
             if (direction[0] != move ) {direction = [-move, 0];} break;
-        case 38 || 87: 
+        case 38: 
             if (direction[1] != move ) {direction = [0, -move];} break;
         case 39: 
             if (direction[0] != -move ) {direction = [move, 0];} break;
         case 40: 
             if (direction[1] != -move ) {direction = [0, move];} break;
+        case 32: clearInterval(run);  newGame(); break;
     }
 } );
 
 const drawSnake =(snake, i)=>{
     cvs.save();
     if (i===0){
-      cvs.fillStyle = 'red';
+      cvs.fillStyle = '#030f17';
     } else {
-        cvs.fillStyle = "green";
+        cvs.fillStyle = "#0a2d47";
     }
-    cvs.fillRect(snake.x+1, snake.y+1, gridSize-2, gridSize-2);
+    cvs.fillRect(snake.x+Math.min(~~(i/3), 7), snake.y+Math.min(~~(i/3), 7), gridSize-2*Math.min(~~(i/3), 7), gridSize-2*Math.min(~~(i/3), 7) );
     cvs.restore();
 };
 
 const makeFood = () =>{
-    let x = Math.floor(Math.random() * 480);
-    x -= x%20;
-    let y = Math.floor(Math.random() * 480);
-    y -= y%20;
+    let x = Math.floor(Math.random() * (Height - gridSize));
+    x -= x%gridSize;
+    let y = Math.floor(Math.random() * (Width - gridSize));
+    y -= y%gridSize;
     food = {x:x, y:y};
     console.log(food);
 }
 
 const drawFood =(food)=>{
     cvs.save();
-    cvs.fillStyle = "orange";
+    cvs.fillStyle = "#FA673F";
     cvs.fillRect(food.x+1, food.y+1, gridSize-2, gridSize-2);
     cvs.restore();
 };
 
 const updateSnake =()=> {
     for (let i=snake.length-1; i >= 0; --i){
-        if (snake[i].x === 0) {snake[i].x = 500}
-        if (snake[i].y === 0) {snake[i].y = 500}
+        if (snake[i].x === 0) {snake[i].x = Height}
+        if (snake[i].y === 0) {snake[i].y = Width}
         if (i === 0){
             snake[i].x = (snake[i].x + direction[0])%Height;
             snake[i].y = (snake[i].y + direction[1])%Width;
@@ -68,17 +68,45 @@ const updateSnake =()=> {
     } 
 } 
 
+const snakeEat=()=> {
+    if (food.x === snake[0].x && food.y === snake[0].y) {
+        snake.unshift(food);
+        makeFood();
+    }
+}
+
+const snakeEatSnake = () =>{
+    for (let i = 1; i <snake.length; ++i ) {
+            if ( snake[i].x === snake[0].x && snake[i].y === snake[0].y )  {
+                clearInterval(run);
+                cvs.fillRect(0,0,Width,Height);
+                cvs.fillStyle = "#000";
+                cvs.clearRect(0,0,Width,Height);
+                console.log("game over");
+                cvs.font = "60px Arial";
+                cvs.fillText("Game Over", 90, 210);
+                cvs.font = "20px Arial";
+                cvs.fillText(`you survived long enough to eat ${snake.length -3} fruits`, 70, 280);
+                cvs.fillText("Press SPACE to try again", 130, 330);
+            }
+    }
+}
+
 const updateBoard =()=> {
-    cvs.clearRect(0,0,Width,Height);    
-    snake.forEach(drawSnake);
+    //cvs.clearRect(0,0,Width,Height); 
+    cvs.fillStyle = "#177A4D";  
+    cvs.fillRect(0,0,Width,Height);
+    snake.forEach(drawSnake);    
+    snakeEatSnake();
+    snakeEat();
     drawFood(food);
     updateSnake();    
 }
 
 const newGame =()=> {
+    snake = [...snakeStart] ;
     makeFood();
-    setInterval(updateBoard,100);    
+    run = setInterval(updateBoard,100);        
 } 
 
 newGame();
-
